a882e85fc1979ed5a6d64b06fdb27f3e29fc5e2e
Anand Vibhuti <mailaddress.anandvibhuti@gmail.com>
2023-07-06 20:44:52 +0530

    added index.css

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,26 @@
-function BookCreate(){
+import {useState} from 'react';
+function BookCreate({onCreate}){
+    const[title,setTitle]=useState('');
+const handleChange=(event)=>{
+setTitle(event.target.value);
+}
+const handleSubmit=(event)=>{
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+
+}
+
+
+
     return(
-        <div>
-            BookCreate
+        <div className="book-create">
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label> Title </label>
+                <input className="input" value={title} onChange={handleChange}></input>
+                <button className='button'>Create!</button>
+            </form>
         </div>
     );
 }
